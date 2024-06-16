9bd88f00b267e40f214fba04e7449b404bc0ac60
husseinjfr <husseinmohammadjaafar@gmail.com>
2024-06-16 13:37:57 +0300

    add updateUser , getAllUsers func

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const { login, createUser } = require("../controllers/userController");
+const {
+  login,
+  createUser,
+  getAllUsers,
+  updateUser,
+} = require("../controllers/userController");
 
+router.get("/getAll", getAllUsers);
 router.post("/create", createUser);
 router.post("/login", login);
+router.patch("/update/:id", updateUser);
 
 module.exports = router;
